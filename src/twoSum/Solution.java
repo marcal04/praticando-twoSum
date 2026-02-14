e8855c67feecde598e001c1186dafbdc1cab71b7
marcal04 <guie.henrique2004@gmail.com>
2026-02-14 20:22:41 -0300

    Add Two Sum solution

---
+++ src/twoSum/Solution.java
@@ -0,0 +1,37 @@
+package twoSum;//Exemplo
+//Entrada: nums = [2,7,11,15], alvo = 9
+// Saída: [0,1]
+// Explicação: Como nums[0] + nums[1] == 9, retornamos [0, 1].
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+
+    public static int[] twoSum(int[] nums, int target) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{};
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+
+        int[] result = twoSum(nums, target);
+
+        System.out.println("[" + result[0] + "," + result[1] + "]");
+
+    }
+}
